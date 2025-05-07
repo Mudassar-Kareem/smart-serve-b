2e878b383852dba5b3eb9d00d50b1025f759e841
Mudassar <mudassarkareem@gmail.com>
2025-05-07 12:21:27 +0500

    .

--- app.js
+++ app.js
@@ -8,26 +8,25 @@ const errorMiddleware = require("./middleware/error");
 
 const app = express();
 const corsOptions = {
-    origin:["http://localhost:5173","https://smartserve-seven.vercel.app"],     
-    optionsSuccessStatus: 200, 
+    origin: ["http://localhost:5173", "https://smartserve-seven.vercel.app"],
+    optionsSuccessStatus: 200,
     credentials: true,
 };
 app.use(cors(corsOptions))
-app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 // Testing API
-app.get("/",(req,res,next)=>{
+app.get("/", (req, res, next) => {
     res.status(200).json({
-        success:true,
-        message:"API is working"
+        success: true,
+        message: "API is working"
     })
 })
 
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/menu",menuRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/menu", menuRouter)
 
 app.use(errorMiddleware)
 
-module.exports ={app}
+module.exports = { app }
